062f5636b824f2f66c3db702eaa1d18bd2fb1326
Nathalie <nathaliebuss21@gmail.com>
2026-01-06 16:32:22 +0100

    added css, changed the layout for the hamburger placement as well as tidy the design a bit more.

--- portfolio.js
+++ portfolio.js
@@ -7,6 +7,7 @@ burgerMenu.addEventListener("click", () => {
   navMenu.classList.toggle("active");
 });
 
+//LOGIC THAT HIDES THE MENU WHEN CLICKED
 navLink.forEach((link) => {
   link.addEventListener("click", () => {
     navMenu.classList.remove("active");
