c645631bc8995cd444484ea6745cf4d82d118f5a
Nathalie <nathaliebuss21@gmail.com>
2026-01-06 15:41:13 +0100

    adding js logic to the hamburger memu.

--- portfolio.js
+++ portfolio.js
@@ -1,6 +1,14 @@
 const burgerMenu = document.getElementById("burger");
 const navMenu = document.getElementById("menu");
+const navLink = document.querySelectorAll(".menu a");
 
+//LOGIC FOR SHOWING THE MENU
 burgerMenu.addEventListener("click", () => {
   navMenu.classList.toggle("active");
+});
+
+navLink.forEach((link) => {
+  link.addEventListener("click", () => {
+    navMenu.classList.remove("active");
+  });
 });
